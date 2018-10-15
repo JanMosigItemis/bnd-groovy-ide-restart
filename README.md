9f481024f281d2aadfe8ef63a632c9f16b7e2875
Jan Mosig <jan.mosig@itemis.de>
2018-10-15 14:02:11 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,1 @@
+A test project to showcase an error when running Groovy+JUnit based integration tests in an OSGi container with bnd+Eclipse.
---
+++ testproject/src/main/groovy/testproject/ExampleTest.groovy
@@ -0,0 +1,15 @@
+package testproject
+
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.runners.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner.class)
+class ExampleTest {
+
+    @Test
+    void 'test some code' () {
+        Assert.assertTrue(true);
+    }
+}
